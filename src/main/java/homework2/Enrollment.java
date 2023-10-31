ed01c9c3cec2e7a4da80fdb4811fee849ed74a28
NiceLeader <maciek2002.10@wp.pl>
2023-10-31 16:31:27 +0100

    Added Enrollment.java

---
+++ src/main/java/homework2/Enrollment.java
@@ -0,0 +1,37 @@
+package homework2;
+
+public class Enrollment {
+    private Student student;
+    private Subject subject;
+    private Integer grade;
+
+    public Enrollment(Student student, Subject subject) {
+        this.student = student;
+        this.subject = subject;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Integer getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+}
+

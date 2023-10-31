a026aeb8768e7f996bb90193c2eef97324be7024
NiceLeader <maciek2002.10@wp.pl>
2023-10-31 16:31:54 +0100

    Added Faculty.java

---
+++ src/main/java/homework2/Faculty.java
@@ -0,0 +1,31 @@
+package homework2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faculty {
+    private String name;
+    private List<Specialization> specializations;
+
+    public Faculty(String name) {
+        this.name = name;
+        this.specializations = new ArrayList<>();
+    }
+
+    public void addSpecialization(Specialization specialization) {
+        specializations.add(specialization);
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Specialization> getSpecializations() {
+        return specializations;
+    }
+}

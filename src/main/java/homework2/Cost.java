ae12f5f0f8424fb293e90ea038408c216e87a3bf
NiceLeader <maciek2002.10@wp.pl>
2023-11-03 13:31:26 +0100

    added discount and scholarship logic to Cost.java

--- src/main/java/homework2/Cost.java
+++ src/main/java/homework2/Cost.java
@@ -5,12 +5,28 @@ public class Cost {
     private double otherFees;
 
     public Cost(double tuition, double otherFees) {
-        this.tuition = tuition;
-        this.otherFees = otherFees;
+        setTuition(tuition);
+        setOtherFees(otherFees);
     }
 
     public double calculateTotalCost() {
         return tuition + otherFees;
+    }
+
+    public double applyScholarship(double scholarshipAmount) {
+        double adjustedTuition = tuition - scholarshipAmount;
+        return adjustedTuition < 0 ? 0 : adjustedTuition;
+    }
+
+    public double applyDiscount(double discountPercentage) {
+        if (discountPercentage < 0 || discountPercentage > 100) {
+            throw new IllegalArgumentException("Discount percentage must be between 0 and 100");
+        }
+        return tuition * (1 - discountPercentage / 100);
+    }
+
+    public String getCostBreakdown() {
+        return "Tuition: $" + tuition + "\nOther Fees: $" + otherFees;
     }
 
     public double getTuition() {
@@ -18,6 +34,9 @@ public class Cost {
     }
 
     public void setTuition(double tuition) {
+        if (tuition < 0) {
+            throw new IllegalArgumentException("Tuition must be non-negative");
+        }
         this.tuition = tuition;
     }
 
@@ -26,6 +45,9 @@ public class Cost {
     }
 
     public void setOtherFees(double otherFees) {
+        if (otherFees < 0) {
+            throw new IllegalArgumentException("Other fees must be non-negative");
+        }
         this.otherFees = otherFees;
     }
 }

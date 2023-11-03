ae950891bbe3077a47e09dee29ecaaa0711af731
NiceLeader <maciek2002.10@wp.pl>
2023-11-03 15:22:52 +0100

    task 2 OOP

--- src/main/java/homework2/Student.java
+++ src/main/java/homework2/Student.java
@@ -1,11 +1,13 @@
+
 package homework2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Student {
-    private String name;
-    private List<Subject> enrolledSubjects;
+    protected String name;  // made protected to use the modifier as required
+    protected List<Subject> enrolledSubjects;  // made protected to use the modifier as required
 
     public Student(String name) {
         this.name = name;
@@ -25,6 +27,28 @@ public class Student {
     }
 
     public List<Subject> getEnrolledSubjects() {
-        return enrolledSubjects;
+        return new ArrayList<>(enrolledSubjects); // Return a copy to protect the encapsulation
+    }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "name='" + name + '\'' +
+            ", enrolledSubjects=" + enrolledSubjects +
+                    '}';
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) &&
+                Objects.equals(enrolledSubjects, student.enrolledSubjects);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, enrolledSubjects);
     }
 }

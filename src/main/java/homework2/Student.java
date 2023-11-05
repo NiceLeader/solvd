45ad26805ca9b13452bc58a7dd5a237753eefd8d
NiceLeader <maciek2002.10@wp.pl>
2023-11-05 03:03:49 +0100

    upgraded Student

--- src/main/java/homework2/Student.java
+++ src/main/java/homework2/Student.java
@@ -5,12 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Student {
-    protected String name;  // made protected to use the modifier as required
-    protected List<Subject> enrolledSubjects;  // made protected to use the modifier as required
+public class Student extends Person {
+    protected List<Subject> enrolledSubjects;
 
     public Student(String name) {
-        this.name = name;
+        super(name);
         this.enrolledSubjects = new ArrayList<>();
     }
 
@@ -18,37 +17,34 @@ public class Student {
         enrolledSubjects.add(subject);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public List<Subject> getEnrolledSubjects() {
-        return new ArrayList<>(enrolledSubjects); // Return a copy to protect the encapsulation
+        return new ArrayList<>(enrolledSubjects);
     }
 
-        @Override
-        public String toString() {
-            return "Student{" +
-                    "name='" + name + '\'' +
-            ", enrolledSubjects=" + enrolledSubjects +
-                    '}';
-        }
+    @Override
+    public String getRole() {
+        return "Student";
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+        ", enrolledSubjects=" + enrolledSubjects +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         Student student = (Student) o;
-        return Objects.equals(name, student.name) &&
-                Objects.equals(enrolledSubjects, student.enrolledSubjects);
+        return Objects.equals(enrolledSubjects, student.enrolledSubjects);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, enrolledSubjects);
+        return Objects.hash(super.hashCode(), enrolledSubjects);
     }
 }

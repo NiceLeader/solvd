e00f01820009c62ae478d62c69802ae4f4fa7c63
NiceLeader <maciek2002.10@wp.pl>
2023-11-05 03:03:21 +0100

    added Person

---
+++ src/main/java/homework2/Person.java
@@ -0,0 +1,20 @@
+
+package homework2;
+
+public abstract class Person {
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String getRole();
+}

1eb333cafa36bad857b60032662ec208cc78e346
NiceLeader <maciek2002.10@wp.pl>
2023-11-07 16:50:50 +0100

    created UniversityUtils

---
+++ src/main/java/homework2/university/UniversityUtils.java
@@ -0,0 +1,26 @@
+package homework2.university;
+
+import homework2.Student;
+
+public class UniversityUtils {
+    private UniversityUtils() {
+    }
+
+    public static double calculateGPA(Student student) {
+        double sumOfGrades = 0.0;
+        int numberOfGrades = 0;
+
+        for (double grade : student.getGrades()) {
+            sumOfGrades += grade;
+            numberOfGrades++;
+        }
+
+        if (numberOfGrades == 0) {
+            return 0.0;
+        }
+
+        double gpa = sumOfGrades / numberOfGrades;
+
+        return Math.round(gpa * 100.0) / 100.0;
+    }
+}

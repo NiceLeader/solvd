ace3b814b2527f9e0f2acecf585928dfed18fcde
NiceLeader <maciek2002.10@wp.pl>
2023-11-07 16:51:47 +0100

    changes

--- src/main/java/homework2/UniversitySystemMain.java
+++ src/main/java/homework2/UniversitySystemMain.java
@@ -13,45 +13,34 @@ public class UniversitySystemMain {
     }
 
     public static void main(String[] args) {
-        // Create a new Student
         Student student = new Student("Maciej Lewandowski");
 
-        // Enroll the student in subjects
         student.enrollInSubject(new Subject("Mathematics", 1));
         student.enrollInSubject(new Subject("History", 2));
 
-        // Process the enrollment using polymorphism
         UniversityManager universityManager = new UniversityManager();
         universityManager.processEnrollment(student);
 
-        // Add grades to the student using the Evaluatable interface
         student.addGrade(90.0);
         student.addGrade(85.5);
 
-        // Handle payments
         student.processPayment(new BigDecimal("1500.00"));
 
         Professor professorSmith = new Professor("Sergey Zagriychuk");
         professorSmith.teachSubject(new Subject("Physics", 1));
         professorSmith.teachSubject(new Subject("Chemistry", 2));
 
-        // Create an AcademicAdvisor and have them counsel students
         AcademicAdvisor advisorJohnson = new AcademicAdvisor("Johnson");
-        advisorJohnson.provideCounseling(student); // Assuming 'student' is already created as shown in previous snippets
+        advisorJohnson.provideCounseling(student);
 
-        // Display taught subjects by Professor Smith
         System.out.println("Professor Smith teaches: " + professorSmith.getTaughtSubjects());
 
-        // Display advised students by Advisor Johnson
         System.out.println("Advisor Johnson advises: " + advisorJohnson.getAdvisedStudents());
 
-        // Use the static method to calculate GPA
         UniversityUtils.calculateGPA(student);
 
-        // Output the total number of enrolled students using the static variable
         System.out.println("Total enrolled students: " + UniversityConfig.totalEnrolledStudents);
 
-        // Demonstrate final variable usage
         System.out.println("University Name: " + UniversityConfig.UNIVERSITY_NAME);
     }
 }

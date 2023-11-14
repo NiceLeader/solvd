e583db465335e8c83abbd59c102598652e936cf6
NiceLeader <maciek2002.10@wp.pl>
2023-11-14 16:50:52 +0100

    usage of EnrollmentException

--- src/main/java/homework2/university/UniversityManager.java
+++ src/main/java/homework2/university/UniversityManager.java
@@ -1,44 +1,42 @@
 package homework2.university;
 
 import homework2.Subject;
+import homework2.exception.EnrollmentException;
 import homework2.interfaces.Enrollable;
 
+import java.util.logging.Logger;
+
 public class UniversityManager {
+    private static final Logger LOGGER = Logger.getLogger(UniversityManager.class.getName());
     private static final int MAX_STUDENTS_PER_SUBJECT = 25;
 
-    public void processEnrollment(Enrollable enrollee) {
+    public void processEnrollment(Enrollable enrollee) throws EnrollmentException {
 
         if (!canEnrollMoreSubjects(enrollee)) {
-            System.out.println("Student cannot enroll in more subjects");
-            return;
+            throw new EnrollmentException();
         }
 
         for (Subject subject : enrollee.getEnrolledSubjects()) {
             if (isSubjectAvailable(subject)) {
-                System.out.println("Student enrolled in subject: " + subject.getName());
+                LOGGER.info("Student enrolled in subject: " + subject.getName());
             } else {
-                System.out.println("Subject is not available: " + subject.getName());
+                throw new EnrollmentException();
             }
         }
 
-
         UniversityConfig.incrementEnrolledStudents();
     }
+
     private boolean canEnrollMoreSubjects(Enrollable enrollee) {
         final int MAX_CREDITS_PER_SEMESTER = 30;
-
         int currentCredits = enrollee.getEnrolledSubjects().stream()
                 .mapToInt(Subject::getCreditPoints)
                 .sum();
-
         return currentCredits <= MAX_CREDITS_PER_SEMESTER;
     }
+
     private boolean isSubjectAvailable(Subject subject) {
-
         int enrolledStudentCount = subject.getEnrolledStudentCount();
-
         return enrolledStudentCount < MAX_STUDENTS_PER_SUBJECT;
     }
-
-
 }

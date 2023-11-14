0126635f5a6a6f0506dd7415b6e2ad919f936df9
NiceLeader <maciek2002.10@wp.pl>
2023-11-14 16:39:04 +0100

    added DataValidationException

---
+++ src/main/java/homework2/exception/DataValidationException.java
@@ -0,0 +1,9 @@
+package homework2.exception;
+
+public class DataValidationException extends Exception {
+
+    public DataValidationException() {
+        super("Data validation failed");
+    }
+
+}

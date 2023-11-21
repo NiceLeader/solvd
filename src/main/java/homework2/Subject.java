df90168de9ad3896cffadc557e1667545cc0bdc4
NiceLeader <maciek2002.10@wp.pl>
2023-11-21 02:12:13 +0100

    Added collections for Subject.java

--- src/main/java/homework2/Subject.java
+++ src/main/java/homework2/Subject.java
@@ -1,20 +1,26 @@
 package homework2;
 
 import homework2.exception.DataValidationException;
+import homework2.person.Student;
 import homework2.university.UniversityConfig;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Subject {
     private String name;
     private int credits;
+    private LinkedList<Student> enrolledStudents;
+
 
 
     public Subject(String name, int credits) {
         this.name = name;
         this.credits = credits;
+        this.enrolledStudents = new LinkedList<>();
     }
+
     public static String subjectsToString(List<Subject> subjects) {
         return subjects.stream()
                 .map(Subject::toString)
@@ -34,6 +40,9 @@ public class Subject {
         }
         this.name = name;
     }
+    public void enrollStudent(Student student) {
+        this.enrolledStudents.add(student);
+    }
 
 
     public int getCredits() {
@@ -46,6 +55,9 @@ public class Subject {
 
     public int getCreditPoints() {
         return credits * 10;
+    }
+    public LinkedList<Student> getEnrolledStudents() {
+        return this.enrolledStudents;
     }
 
     public int getEnrolledStudentCount() {
